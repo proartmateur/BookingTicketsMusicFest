acfaa2537a4e8187d6789a830b99108952aa3726
proartmateur <ennimandroid@gmail.com>
2022-06-30 02:28:39 -0500

    Fix controller

--- index.ts
+++ index.ts
@@ -5,21 +5,27 @@ import {
 } from "./src/TicketmasterCtx/Concierto/Infrastructure/Persistence/ConciertoMemoryRepository";
 import {ConciertoDayAdder} from "./src/TicketmasterCtx/Concierto/UseCases/ConciertoDayAdder";
 import {ConciertoFinder} from "./src/TicketmasterCtx/Concierto/UseCases/ConciertoFinder";
+import {ConciertoCreator} from "./src/TicketmasterCtx/Concierto/UseCases/ConciertoCreator";
 
-const repo = new ConciertoMemoryRepository();
+(async ()=>{
+  const repo = new ConciertoMemoryRepository();
 
 // Obtener del exterior los datos del concierto
 const concierto_name = "MF 23";
 const concierto_place = "Auditorio BlackBerry";
 const day_1 = {date: "", seats: 50};
 
+const creator = new ConciertoCreator(repo);
+await creator.run(concierto_name, concierto_place);
+
 // Cuerpo del controller
 const adder = new ConciertoDayAdder(repo);
-adder.run(concierto_name, {date: "", seats: 50})
+await adder.run(concierto_name, {date: "", seats: 50})
 
 const finder = new ConciertoFinder(repo);
 const saved_concierto = await finder.run(concierto_name);
 
 console.log(saved_concierto)
+})();
 
 
